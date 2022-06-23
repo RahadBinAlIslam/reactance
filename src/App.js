69c3a95aec1a96ea72fb2aac4dc5d1c612109120
R4H4D <rahadbinalislam@gmail.com>
2022-06-23 13:32:14 +0600

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   const title = "Welcome to the new blog";
@@ -10,9 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
+        <Home />
       </div>
     </div>
   );
