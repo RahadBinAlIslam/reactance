e42a79cdb52b25cd4cfbb83c140cd5fb91421392
R4H4D <rahadbinalislam@gmail.com>
2022-06-23 12:40:05 +0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const title = 'Welcome to the new blog';
+  const title = "Welcome to the new blog";
   const likes = 50;
   //const person = {name: 'yoshi', age: 30};
   return (
